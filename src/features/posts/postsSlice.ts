da7c985dfc754199d1879a034e178805035b31cb
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-06-19 16:04:40 +0800

    added posts slice

---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export class Post {
+    id: number;
+    title: string;
+    content: string;
+
+    constructor(id: number, title: string, content: string) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
+
+export interface PostsState {
+    posts: Post[];
+    status: 'idle' | "pending" | "error" | "success";
+}
+
+const initialState: PostsState = {
+    posts: [
+        new Post(1, 'Post 1', 'Content 1'),
+        new Post(2, 'Post 2', 'Content 2'),
+    ],
+    status: 'idle',
+}
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {}
+})
+
+export default postsSlice.reducer
